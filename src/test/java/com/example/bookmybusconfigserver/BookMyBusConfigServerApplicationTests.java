fb8b2e5388dcc294496217f10cf1b329eac886c0
deena <deenaantonym@gmail.com>
2024-05-09 07:17:36 +0530

    Initial commit. Created config server. Added dependencies and properties.

---
+++ src/test/java/com/example/bookmybusconfigserver/BookMyBusConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmybusconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyBusConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
